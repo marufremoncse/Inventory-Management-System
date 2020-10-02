b5f0907cf9fcb3c5dbca7e4ee67dc8c6eb093a9a
marufremoncse <marufremon.cse14.2@gmail.com>
2020-10-02 11:36:00 +0600

    Changes done in customer_create.blade.php

--- resources/views/admin/customer/customer_create.blade.php
+++ resources/views/admin/customer/customer_create.blade.php
@@ -20,7 +20,7 @@
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
                         <li class="breadcrumb-item"><a href="{{route('dashboard')}}">Dashboard</a></li>
-                        <li class="breadcrumb-item active"> {{$page_name}}</li>
+                        <li class="breadcrumb-item active">{{$page_name}}</li>
                     </ol>
                 </div>
             </div>
